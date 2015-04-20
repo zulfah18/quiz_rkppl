0ab30c2b56fb6ae46806fee36b15a5bfe26618b1
zulfahnur <zulfahnur.18@mail.unpas.ac.id>
2015-04-20 12:06:03 +0700

    versi 4

---
+++ src/test/java/tambah/tambahan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tambah;
+
+/**
+ *
+ * @author SB6 604-12
+ */
+public class tambahan {
+    
+    public int tambah(int  bilA, int bilB) {
+        return bilA + bilB;
+    }
+    
+}
